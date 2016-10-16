577281615b6e0629523809766ad08bba801d11ba
Muhammad Mudassir Hassan <mudassirhassan.27@hotmail.com>
2016-10-16 09:18:53 +0500

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Apifirst
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.EnableCors();
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling
+            = Newtonsoft.Json.PreserveReferencesHandling.None;//Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
+            = ReferenceLoopHandling.Ignore;
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apifirst.Models
+{
+   // [Table("Student")]
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Class { get; set; }
+    }
+    public class StudentdbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
